9d7dccf98b6fe284d92cd5480de4728bf3e9e1fc
vierigata27 <vierigata27@gmail.com>
2023-09-11 12:30:06 +0700

    Latihan JS Pertemuan 4

---
+++ skirp.js
@@ -0,0 +1,15 @@
+function dataperson() {
+    let forms = document.getElementById('frm');
+    let nama = forms.nama.value;
+    let pekerjaan = forms.pekerjaan.value;
+    let hobby = forms.hobby.value;
+
+    let data = ` 
+        Input Data 
+        <br> Nama : ${nama}
+        <br> Pekerjaan : ${pekerjaan}
+        <br> Hobby : ${hobby}
+    `;
+
+    document.getElementById('hasil').innerHTML = data;
+}
