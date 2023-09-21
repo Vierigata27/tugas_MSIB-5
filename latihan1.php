d63d19e31bec7a4d4112fe9853671ab274b6d46c
vierigata27 <vierigata27@gmail.com>
2023-09-21 12:08:17 +0700

    Pertemuan 1 PHP

---
+++ latihan1.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Vieri Arief Maulana";
+$umur = 20;
+$berat = 60;
+$pekerjaan = "Mahasiswa";
+
+echo '<br>Nama : ' .$nama ;
+echo '<br>Umur : ' .$umur. ' tahun' ;
+echo '<br>Berat Badan : ' .$berat. ' KG';
+echo '<br>Pekerjaan : ' .$pekerjaan ;
+?>
